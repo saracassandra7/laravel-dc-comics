62bc4b0200006ab0e275bfd488567ac29e7be5f6
saracassandra7 <saracassandra7@gmail.com>
2023-01-13 17:15:28 +0100

    [add] destroy

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,6 +5,14 @@
     <h1 class="text-center m-3">Comics dashboard</h1>
     <a class="btn btn-info mb-5" href="{{route('comics.create')}}">Crea nuovo fumetto</a>
 
+    @if (session('deleted'))
+        <div>
+            <div class="alert alert-success" role="alert">
+                {{session('deleted')}}
+            </div>
+        </div>
+    @endif
+
     <table class="table table-striped">
         <thead>
           <tr>
@@ -23,11 +31,19 @@
                  <td>
                   <a class="btn btn-primary" href="{{route('comics.show', $comic)}}" title="show"><i class="fa-regular fa-eye"></i></a>
                   <a class="btn btn-warning " href="{{route('comics.edit', $comic)}}" title="edit"><i class="fa-solid fa-pencil"></i></a>
+                  <form class="d-inline"
+                  onsubmit="return confirm('Confermi l\'eliminazione di: {{$comic->title}}')"
+                  action="{{route('comics.destroy', $comic)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                        <button type="submit" class="btn btn-danger " title="delete"><i class="fa-solid fa-trash"></i></button>
+                  </form>
 
                 </td>
              </tr>
 
             @empty
+              <h1>Nessun fumetto trovato</h1>
 
             @endforelse
         </tbody>
@@ -35,7 +51,5 @@
 
 
 </div>
-
-
 
 @endsection
